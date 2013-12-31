56d2a85a0219ebb243e543ba492c4ca5fe9b581a
otknoy <otknoy@gmail.com>
2013-12-31 17:47:00 +0900

    overlay multi charts

--- js/RadarChart.js
+++ js/RadarChart.js
@@ -1,4 +1,4 @@
-function drawRadarChart(data) {
+function drawRadarChart(labels, data, maxValue) {
     var width = 480;
     var height = 480;
     var centerX = width  / 2;
@@ -10,93 +10,79 @@ function drawRadarChart(data) {
      func: Math.cos or Math.sin
      */
     function getPosition(i, r, func) {
-	var rad = i/valueList.length * 2*Math.PI;
+	var rad = i/labels.length * 2*Math.PI;
 	return r*func(rad - Math.PI/2);
     }
     function getPositionX(i, r){return centerX + getPosition(i, r, Math.cos);}
     function getPositionY(i, r){return centerY + getPosition(i, r, Math.sin);}
 
-    var axisList = data.map(function(d){return d.axis;});
-    var valueList = data.map(function(d){return d.value;});
-    var maxValue = Math.max.apply(null, valueList);
-
+    // var maxValue = Math.max.apply(null, Array.prototype.concat.apply([], data));
 
     // svg
     var svg = d3.select('div.chart')
 	    .append('svg')
 	    .attr('width', width)
 	    .attr('height', height);
-
-    // memo: n loop
-    // around
-    // var aroundLine = d3.svg.line()
-    // 	    .x(function(d, i) { return getPositionX(i, r);})
-    // 	    .y(function(d, i) { return getPositionY(i, r);})
-    // 	    .interpolate("linear-closed");
-    // var around = svg.append("path")
-    // 	    .attr("d", aroundLine(data))
-    // 	    .style("stroke", "black")
-    // 	    .style("fill", "none");
-    var around = svg.append("circle")
-	    .attr("cx", centerX)
-	    .attr("cy", centerY)
-	    .attr("r", r)
-    	    .style("stroke", "black")
-            .style("fill", "gray")
-	    .style("opacity", 0.333);
+    svg.append("circle")
+	.attr("cx", centerX)
+	.attr("cy", centerY)
+	.attr("r", r)
+    	.style("stroke", "black")
+        .style("fill", "gray")
+	.style("opacity", 0.333);
 
     // axis
-    var axis = svg.selectAll("line")
-	    .data(axisList)
-	    .enter().append("line")
-    	    .attr("x1", function(d, i){return width/2;})
-	    .attr("y1", function(d, i){return height/2;})
-	    .attr("x2", function(d, i){return getPositionX(i, r);})
-	    .attr("y2", function(d, i){return getPositionY(i, r);})
-	    .style("stroke", "gray");
+    svg.selectAll("line")
+	.data(labels)
+	.enter().append("line")
+    	.attr("x1", function(d, i){return width/2;})
+	.attr("y1", function(d, i){return height/2;})
+	.attr("x2", function(d, i){return getPositionX(i, r);})
+	.attr("y2", function(d, i){return getPositionY(i, r);})
+	.style("stroke", "gray");
     // axis label
-    var axisLabel = svg.selectAll("a")
-    	    .data(data)
-    	    .enter().append("a")
-	    .attr("class", "label")
-	    .attr("xlink:href", function(d){return d.url;})
-	    .append("text")
-    	    .text(function(d){return d.axis;})
-    	    .attr("text-anchor", "middle")
-    	    .attr("x", function(d, i){return getPositionX(i, 1.1*r);})
-    	    .attr("y", function(d, i){return getPositionY(i, 1.1*r);});
+    svg.selectAll("text")
+	.data(labels)
+	.enter().append("text")
+    	.text(function(d){return d;})
+    	.attr("text-anchor", "middle")
+    	.attr("x", function(d, i){return getPositionX(i, 1.1*r);})
+    	.attr("y", function(d, i){return getPositionY(i, 1.1*r);});
 
-    // path
-    var line = d3.svg.line()
-            .x(function(d, i) { return getPositionX(i, (d.value/maxValue)*r);})
-            .y(function(d, i) { return getPositionY(i, (d.value/maxValue)*r);})
-            .interpolate("linear-closed");
-    var valueGraph = svg.append("path")
-            .attr("d", line(data))
-            .style("stroke", "blue")
-            .style("stroke-width", 2)
-            .style("fill", "blue")
+    // plot data
+    var colors =  ["red", "green", "blue"];
+    for (var j = data.length-1; 0 <= j; j--) {
+	var g = svg.append("g");
+
+	// path
+	var line = d3.svg.line()
+		.x(function(d, i) { return getPositionX(i, (d/maxValue)*r);})
+		.y(function(d, i) { return getPositionY(i, (d/maxValue)*r);})
+		.interpolate("linear-closed");
+	g.append("path")
+	    .attr("d", line(data[j]))
+	    .style("stroke", colors[j])
+	    .style("stroke-width", 2)
+	    .style("fill", colors[j])
 	    .style("opacity", 0.5);
 
-    // point
-    var point = svg.selectAll("circle.point")
-    	    .data(data)
+	// poitn
+	g.selectAll("circle")
+    	    .data(data[j])
     	    .enter().append("circle").attr("class", "point")
-    	    .attr("cx", function(d, i){return getPositionX(i, (d.value/maxValue)*r);})
-    	    .attr("cy", function(d, i){return getPositionY(i, (d.value/maxValue)*r);})
+    	    .attr("cx", function(d, i){return getPositionX(i, (d/maxValue)*r);})
+    	    .attr("cy", function(d, i){return getPositionY(i, (d/maxValue)*r);})
     	    .attr("r", 3)
-    	    .style("fill", "blue");
+    	    .style("fill", colors[j]);
+    }
 }
 
 $(function() {
-    var data = [
-        {axis: "1群", value: 13, url: "http://www.google.com"}, 
-        {axis: "2群", value: 1 , url: "http://amateras.wsd.kutc.kansai-u.ac.jp"}, 
-        {axis: "3群", value: 8 , url: "http://amateras.wsd.kutc.kansai-u.ac.jp/hiki"},  
-        {axis: "4群", value: 4 , url: "http://amateras.wsd.kutc.kansai-u.ac.jp/yp"},
-        {axis: "5群", value: 9 , url: "http://www.kansai-u.ac.jp/index.html"},
-        {axis: "6群", value: 9 , url: "http://www.kansai-u.ac.jp/Fc_inf/"}
-    ];
-
-    drawRadarChart(data);
+    var labels = ["1群", "2群", "3群", "4群", "5群", "6群"];
+    var data = [[13, 1, 8, 4, 9, 9],
+		[13, 7, 9, 10, 9, 9],
+		[15, 10, 15, 10, 15, 14]];
+    var maxValue = 15;
+    
+    drawRadarChart(labels, data, maxValue);
 });
